af54e8a789b3ce1143ab40a661db52856e7db6ac
Rands-mkz <117436733+Rands-mkz@users.noreply.github.com>
2023-01-18 22:11:12 +0800

    Add files via upload

---
+++ FILE.py
@@ -0,0 +1,17 @@
+import os, platform, time, sys
+
+'''
+46HCRAA7414410223322SSB
+46HCRAA7414410223322SSB
+46HCRAA7414410223322SSB
+'''
+
+print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m64Bit Found')
+ import FILE64
+elif bit == '32bit':
+ print('\033[1;91m[\033[1;97m✓\033[1;91m] \033[1;97m32Bit Found')
+ import FILE32
